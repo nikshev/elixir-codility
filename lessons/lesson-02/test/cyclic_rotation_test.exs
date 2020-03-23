bfdd0231889817956410afdec9c68100c0e78849
nikshev81 <nikshev81@gmail.com>
2020-03-23 16:51:24 +0200

    Initial commit

---
+++ lessons/lesson-02/test/cyclic_rotation_test.exs
@@ -0,0 +1,8 @@
+defmodule CyclicRotationTest do
+  use ExUnit.Case
+  doctest CyclicRotation
+
+  test "greets the world" do
+    assert CyclicRotation.hello() == :world
+  end
+end
---
+++ lessons/lesson-02/test/odd_occurrences_in_array.exs
@@ -0,0 +1,8 @@
+defmodule OddOccurrencesInArrayTest do
+  use ExUnit.Case
+  doctest OddOccurrencesInArray
+
+  test "greets the world" do
+    assert OddOccurrencesInArray.hello() == :world
+  end
+end
